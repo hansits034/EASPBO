e49a71607fd81f3554cfeaa1a87b54563e8f7d92
Junathan Richie <jonathanrichie05@gmail.com>
2024-12-15 19:57:55 +0700

    refactor: remove unnecessary import

--- Connect5-master/src/Connect5Client.java
+++ Connect5-master/src/Connect5Client.java
@@ -3,12 +3,10 @@ import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Scanner;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.text.NumberFormat.Style;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
